2b5f0f4a61ad5ed0711c53026923e7d314df2040
Saikiran Seelam <skirans2307@gmail.com>
2025-02-17 17:59:11 +0530

    variable commit

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Ram:: Hi Seeta, How are you?"
-echo "Seeta:: Hello Ram. I am fine. How are you doing?"
-echo "Ram:: I am doing good Seeta. What's going on?"
-echo "Seeta:: I started learning shell script Ram"
+PERSON1=Ram
+PERSON2=Seeta
+
+echo "$PERSON1:: Hi ${PERSON2}, How are you?"
+echo "${PERSON2}:: Hello $PERSON1. I am fine. How are you doing?"
+echo "$PERSON1:: I am doing good ${PERSON2}. What's going on?"
+echo "${PERSON2}:: I started learning shell script $PERSON1"
