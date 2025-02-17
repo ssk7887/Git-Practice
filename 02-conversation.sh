32a0590e90ca4af1f2b13ff627cc3c00f0518c6f
Saikiran Seelam <skirans2307@gmail.com>
2025-02-17 17:46:22 +0530

    next commit

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ram:: Hi Seeta, How are you?"
+echo "Seeta:: Hello Ram. I am fine. How are you doing?"
+echo "Ram:: I am doing good Seeta. What's going on?"
+echo "Seeta:: I started learning shell script Ram"
