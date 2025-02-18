63c50b3d59e4a24699309d26244a3860f102a20c
Saikiran Seelam <skirans2307@gmail.com>
2025-02-18 16:14:36 +0530

    colors added

---
+++ 13-colors.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run the script with root privileges"
+        exit 1
+        fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is..$R FAILED $N"
+        exit 1
+    else
+        echo "$2 is..$G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT
+
+dnf list installed git # Just checking whether installed or not
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y # here installing
+    VALIDATE $? "Installing Git"
+else
+    echo "Git is already installed, nothing to do.."
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed..going to install"
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+else
+    echo "MySQL is already Installed, Nothing to do.."
+fi
